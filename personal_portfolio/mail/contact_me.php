a8e4ebce68555e25c4d344d38e73e69dec02330b
Marek Marusin <marusin.marek@gmail.com>
2018-03-20 19:56:10 +0100

    phpmailer lib + working email form

--- personal_portfolio/mail/contact_me.php
+++ personal_portfolio/mail/contact_me.php
@@ -1,26 +1,58 @@
 <?php
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require '../vendor/autoload.php';
+
 // Check for empty fields
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
-   empty($_POST['message'])	||
+if(empty($_POST['name'])        ||
+   empty($_POST['email'])       ||
+   empty($_POST['phone'])       ||
+   empty($_POST['message']) ||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
-	echo "No arguments Provided!";
-	return false;
-   }
-	
+    echo "No arguments Provided!";
+    echo $name;
+    echo $email;
+    echo $phone;
+    echo $message;
+    return false;
+}
+
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
-	
-// Create the email and send the message
-$to = 'marusin.marek@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@meetz.sk\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
-return true;			
+
+
+$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp.websupport.sk';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'marusin@meetz.sk';                 // SMTP username
+    $mail->Password = 'here_type_your_password';                       // SMTP password
+    $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 465;                                    // TCP port to connect to
+    $mail->CharSet = 'UTF-8';
+
+    //Recipients
+    $mail->setFrom($email_address, $name);
+    $mail->addAddress("marusin@meetz.sk", 'Marek Marusin');  
+
+    //Content
+    $mail->isHTML(false);                                  // Set email format to HTML
+    $mail->Subject = "contact form marusin.sk:  $name";
+    $mail->Body    = "You have received a new message from your website contact form.\r\n\n"."Here are the details:\r\nName: $name\r\nEmail: $email_address\r\nPhone: $phone\r\nMessage:\r\n\n$message";
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+}
 ?>
